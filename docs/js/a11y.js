5ed6b945ff8bdf8f7c43a33366f42bd60686f010
Bluet Park <1983bluet@gmail.com>
2022-12-07 17:35:38 +0900

    tab 키로 탭 버튼을 빠져나갈 때 활성화 된 탭 버튼에 tabindex 세팅

--- docs/js/a11y.js
+++ docs/js/a11y.js
@@ -10,30 +10,35 @@ $(function(){
       var welTabBtn = welWrap.find('[role=tab]');
       var welTabPanel = welWrap.find('[role=tabpanel]');
       var nTabFocus = 0;
+      var nTabActive = 0;
       welTabList
-        .on('keydown', '[role=tab]', function(e){
-          if ( e.keyCode === 37 || e.keyCode === 39 ) {
-            $(welTabBtn[nTabFocus]).attr('tabindex','-1');
-            // 왼쪽 방향키
-            if ( e.keyCode === 37 ) {
-              nTabFocus--;
-              if ( nTabFocus < 0 ) { nTabFocus = welTabBtn.length - 1 }
-            }
-            // 오른쪽 방향키
-            if ( e.keyCode === 39 ) {
-              nTabFocus++;
-              if ( nTabFocus >= welTabBtn.length ) { nTabFocus = 0 }
-            }
-            $(welTabBtn[nTabFocus]).attr('tabindex','0');
-            $(welTabBtn[nTabFocus]).trigger('focus');
-          }
-        })
         .on('click', '[role=tab]', function(e){
           nTabFocus = welTabBtn.index($(this)) ;
+          nTabActive = nTabFocus;
+          console.log(nTabFocus, nTabActive)
           welTabBtn.attr({'aria-selected':'false','tabindex':'-1'});
           $(this).attr({'aria-selected':'true','tabindex':'0'});
           welTabPanel.hide().removeAttr('tabindex');
           welTabPanel.eq(nTabFocus).show().attr('tabindex','0');
+        })
+        .on('keydown', '[role=tab]', function(e){
+          if ( e.keyCode === 9 || e.keyCode === 37 || e.keyCode === 39 ) {
+            $(welTabBtn[nTabFocus]).attr('tabindex','-1');
+            // Left Arrow
+            if ( e.keyCode === 37 ) {
+              nTabFocus--;
+              if ( nTabFocus < 0 ) { nTabFocus = welTabBtn.length - 1; }
+            }
+            // Right Arrow
+            if ( e.keyCode === 39 ) {
+              nTabFocus++;
+              if ( nTabFocus >= welTabBtn.length ) { nTabFocus = 0; }
+            }
+            // Tab
+            if ( e.keyCode === 9 ) { nTabFocus = nTabActive; }
+            $(welTabBtn[nTabFocus]).attr('tabindex','0');
+            $(welTabBtn[nTabFocus]).trigger('focus');
+          }
         });
     });
   }
